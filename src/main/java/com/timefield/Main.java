4292360f464c7462a973ee455e427774edf86b1c
Tyziryx <alexim13550@gmail.com>
2023-02-27 20:20:04 +0100

    Setup 1st file

--- src/main/java/com/timefield/Main.java
+++ src/main/java/com/timefield/Main.java
@@ -1,4 +1,6 @@
-package com.timefield;public class Main {
+package com.timefield;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
